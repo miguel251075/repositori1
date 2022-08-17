f87634e90374dbdb14493a0b8ed46b4649c6641b
miguel251075 <109634932+miguel251075@users.noreply.github.com>
2022-08-17 00:21:44 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+//$("h1").hide();
+
+$(document).ready(function(){
+
+     $("p").css({"background-color": "red"});
+     $("#parrafo2").css({"backgroup-color": "green"});
+     $(".parrafo1").css({"background-color": "yellow"});
+     $("button").click(function(){
+        alert("Bienvenidos al taller de programacion")
+     });
+
+
+});
